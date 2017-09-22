ff5d79e73642e34560f3ebd8f7a4c4f0ee9f200e
Dekei <nathancollard1@gmail.com>
2017-09-22 17:27:48 +0200

    Ajout de la base du site

--- Index/src/OC/IndexBundle/OCIndexBundle.php
+++ Index/src/OC/IndexBundle/OCIndexBundle.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace AppBundle;
+namespace OC\IndexBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class AppBundle extends Bundle
+class OCIndexBundle extends Bundle
 {
 }
